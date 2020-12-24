28db2d30284d25197c1697e09fbec65de1eb3ef8
Suvam Basak <64788104+scodebox@users.noreply.github.com>
2020-12-24 16:36:10 +0530

    Update script.py

--- note/script.py
+++ note/script.py
@@ -14,6 +14,18 @@ print(x(10,20))
 from functools import reduce
 reduce(lambda x, y : x + y,[1,2,3])
 
-# define an initial value
+# define an initial value  https://stackoverflow.com/questions/409370/sorting-and-grouping-nested-lists-in-python?answertab=votes#tab-top
 reduce(lambda x, y : x + y, [1,2,3], -3)
 
+# Sorting and Grouping Nested Lists in Python
+x = [
+ ['4', '21', '1', '14', '2008-10-24 15:42:58'], 
+ ['3', '22', '4', '2somename', '2008-10-24 15:22:03'], 
+ ['5', '21', '3', '19', '2008-10-24 15:45:45'], 
+ ['6', '21', '1', '1somename', '2008-10-24 15:45:49'], 
+ ['7', '22', '3', '2somename', '2008-10-24 15:45:51']
+]
+
+from operator import itemgetter
+
+x.sort(key=itemgetter(1))
