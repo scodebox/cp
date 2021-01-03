5ef947f950fe7521256edd149e98edfb5fef895c
suvambasak <suvambasak2008@gmail.com>
2021-01-03 23:23:25 +0530

    format

--- python3/array/1_reverse_of_array.py
+++ python3/array/1_reverse_of_array.py
@@ -1,13 +1,14 @@
-def reverse(l,start,end):
-    if start>=end:
+def reverse(l, start, end):
+    if start >= end:
         return
-    temp=l[start]
-    l[start]=l[end]
-    l[end]=temp
+    temp = l[start]
+    l[start] = l[end]
+    l[end] = temp
 
-    reverse(l,start+1,end-1)
+    reverse(l, start+1, end-1)
+
 
 if __name__ == "__main__":
     l = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10]
-    reverse(l,0,len(l)-1)
-    print (l)
+    reverse(l, 0, len(l)-1)
+    print(l)
