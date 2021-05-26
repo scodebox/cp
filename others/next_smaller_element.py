0c7e3743668f20203e9d140ae0427cf7f45575df
suvambasak <suvambasak2008@gmail.com>
2021-05-26 13:04:36 +0530

    Nearest smaller to left & right

---
+++ others/next_smaller_element.py
@@ -0,0 +1,45 @@
+def is_empty(stack):
+    if len(stack):
+        return False
+    return True
+
+
+def push(stack, item):
+    stack.append(item)
+
+
+def pop(stack):
+    if not is_empty(stack):
+        return stack.pop()
+    return None
+
+
+def top(stack):
+    if len(stack):
+        return stack[-1]
+    return None
+
+
+def next_largest_element(arr):
+    stack = []
+    result = []
+    for i in range(len(arr)-1, -1, -1):
+        while (not is_empty(stack) and (top(stack) >= arr[i])):
+            pop(stack)
+
+        if is_empty(stack):
+            result.append(-1)
+        else:
+            result.append(top(stack))
+        push(stack, arr[i])
+
+    result.reverse()
+    return result
+
+
+if __name__ == '__main__':
+    arr1 = [4, 5, 2, 10, 8]  # >> 2 2 -1 8 -1
+    arr2 = [1, 3, 2, 4]  # >> -1 2 -1 -1
+
+    print(next_largest_element(arr1))
+    print(next_largest_element(arr2))
